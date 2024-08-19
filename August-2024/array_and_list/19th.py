8b801c102bfcb9e746f58bbd06cb3e1c03a1988b
barman_ji_vijay <vbarman116@gmail.com>
2024-08-19 23:26:24 +0530

    19th august 2024

---
+++ August-2024/array_and_list/19th.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+
+solution = Solution()
+
+print(solution.topKFrequent(
+    nums=[1, 2, 2, 3, 3, 3], k=2))
