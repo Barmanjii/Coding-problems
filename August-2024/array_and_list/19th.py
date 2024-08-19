d5d5f77adcde22106cec73952c41b8c95b6d8e8a
barman_ji_vijay <vbarman116@gmail.com>
2024-08-20 00:37:51 +0530

    solved the longestCommonPrefix problem

--- August-2024/array_and_list/19th.py
+++ August-2024/array_and_list/19th.py
@@ -18,8 +18,24 @@ class Solution:
 
         return max_profit
 
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # https://leetcode.com/problems/longest-common-prefix/submissions/1361691360/
+        sorted_string = sorted(strs)
+
+        first_element = sorted_string[0]
+        last_element = sorted_string[-1]
+
+        longest_prefix = ""
+
+        for i in range(len(first_element)):
+            if first_element[i] != last_element[i]:
+                break
+
+            longest_prefix = longest_prefix + first_element[i]
+        return longest_prefix
+
 
 solution = Solution()
 
-print(solution.maxProfit(
-    prices=[1, 2]))
+print(solution.longestCommonPrefix(
+    strs=["a"]))
